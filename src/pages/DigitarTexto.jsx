1ec4a03939ce768743e50910df0de6583bd207f9
Marcios <marciosunico18@gmail.com>
2026-03-12 20:43:46 -0300

    ajustes

--- src/pages/DigitarTexto.jsx
+++ src/pages/DigitarTexto.jsx
@@ -220,7 +220,7 @@ export default function DigitarTexto() {
 
 
     return (
-        <div className="h-dvh flex flex-col  bg-slate-100 digitar-texto px-6">
+        <div className="h-dvh flex flex-col  bg-slate-100 digitar-texto px-6 pb-5">
             <div className="flex-1 overflow-y-auto scrollbar-hide pt-2">
                 <div className="relative text-left mb-4">
                     <div

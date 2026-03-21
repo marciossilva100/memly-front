2dd011e2f668cd5955875fd99731464cc8545169
Marcios <marciossilva.dev@gmail.com>
2026-03-20 23:09:08 -0300

    ajustes

--- src/pages/treinoIA.jsx
+++ src/pages/treinoIA.jsx
@@ -77,13 +77,13 @@ export default function TreinoIA() {
     }
 
     return (
-        <div className="h-[100dvh] flex flex-col items-center  px-4 gap-4 ">
+        <div className="h-[100dvh] flex flex-col items-center  px-4 gap-4 from-gray-900 to-gray-800 bg-gradient-to-br">
             <div className="relative text-left w-full mt-3">
                 <div
                     className="left-0  cursor-pointer"
                     onClick={() => navigate(-1)}
                 >
-                    <i className="bi bi-arrow-left text-2xl"></i>
+                    <i className="bi bi-arrow-left text-2xl text-white"></i>
                 </div>
             </div>
             <div>
@@ -92,21 +92,21 @@ export default function TreinoIA() {
                     onClick={() => setFlipped(!flipped)}
                 >
                     <div className={`flashcard ${flipped ? "flipped" : ""} `}>
-                        <div className="relative py-20 bg-default-gradient text-white shadow-md rounded-lg flex items-center justify-center p-6 text-center text-white-700">
+                        <div className="relative py-20 bg-[linear-gradient(to_right,#233245,#0d1425)] text-white shadow-md rounded-lg flex items-center justify-center p-6 text-center text-white-700">
                             <p className="text-2xl md:text-2xl font-medium ">
                                 {textoTraduzido}
                             </p>
                             <div className="absolute right-0 top-0 mt-3 me-3">
-                                <RefreshCw />
+                                <RefreshCw  className="text-white"/>
 
                             </div>
                         </div>
-                        <div className="relative py-20 back rounded-lg flex items-center justify-center p-6 text-center bg-white">
-                            <p className="text-2xl md:text-2xl font-medium">
+                        <div className="relative py-20 back rounded-lg flex items-center justify-center p-6 text-center bg-[linear-gradient(to_right,#0d1425,#233245)]">
+                            <p className="text-2xl md:text-2xl font-medium text-white">
                                 {textoNativo}
                             </p>
                               <div className="absolute right-0 top-0 mt-3 me-3">
-                                <RefreshCw />
+                                <RefreshCw className="text-white"/>
 
                             </div>
                         </div>

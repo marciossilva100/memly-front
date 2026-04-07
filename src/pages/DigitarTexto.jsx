d776be68a5ef7f8f43b32794f409554197d5a4d7
Marcios <marciossilva.dev@gmail.com>
2026-04-07 11:49:20 -0300

    ajustes

--- src/pages/DigitarTexto.jsx
+++ src/pages/DigitarTexto.jsx
@@ -457,7 +457,7 @@ export default function DigitarTexto() {
                                     ref={textareaRef}
                                     value={resposta}
                                     onChange={(e) => setResposta(e.target.value)}
-                                    className="text-xl text-white toutline-none w-full h-[100%] pt-6 text-center rounded-lg  resize-none bg-gray-800/50 backdrop-blur-sm  border border-gray-700"
+                                    className="text-xl text-white toutline-none w-full h-[100%] pt-6 text-center rounded-lg  resize-none bg-gray-800/50 backdrop-blur-sm  border border-gray-700 px-3"
                                 />
 
 

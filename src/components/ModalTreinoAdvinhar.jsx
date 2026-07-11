a3884b75c641cd0eb4570f0c2aa38230bb11122c
Marcios <marciossilva.dev@gmail.com>
2026-07-10 22:34:08 -0300

    ajusets

--- src/components/ModalTreinoAdvinhar.jsx
+++ src/components/ModalTreinoAdvinhar.jsx
@@ -25,7 +25,7 @@ export default function ModalTreinoAdvinhar({ setOpenTreinoAdvinhar, openTreinoA
             <div className="fixed inset-0 flex items-center justify-center p-2">
                 <Dialog.Panel className="w-60 max-w-xl rounded-2xl from-gray-900 to-gray-800 bg-gradient-to-br p-6 shadow-xl ">
 
-                    <div className="flex  gap-2 items-center space-x-15 mb-4">
+                   {/* <div className="flex  gap-2 items-center space-x-15 mb-4">
                         <div className="flex justify-center items-center me-3">
                             <Eye width={38} height={38} className="text-blue-400" />
 
@@ -33,7 +33,7 @@ export default function ModalTreinoAdvinhar({ setOpenTreinoAdvinhar, openTreinoA
                         <div className="flex flex-col">
                             <span className="text-lg text-white ">Advinhar</span>
                         </div>
-                    </div>
+                    </div>*/}
                     <div className="flex  gap-2 items-center space-x-15 mb-4">
                         <div className="flex justify-center items-center me-3">
                             <Shuffle width={38} height={38} className="text-purple-400" />

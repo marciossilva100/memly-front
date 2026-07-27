440be8c7b5fb19572abc2d9bf00f4a84fd2f62bb
Marcios <marciossilva.dev@gmail.com>
2026-07-27 20:01:40 -0300

    Revert "Corrige ícone do PWA aparecendo com fundo quadrado branco dentro de um blob preto"
    This reverts commit 8e34d885d9779c4bca3771637d9d6df8e9d584fd.

--- vite.config.js
+++ vite.config.js
@@ -110,26 +110,12 @@ export default defineConfig(({ mode }) => {
             {
               src: "/icon-192.png",
               sizes: "192x192",
-              type: "image/png",
-              purpose: "any"
+              type: "image/png"
             },
             {
               src: "/icon-512.png",
               sizes: "512x512",
-              type: "image/png",
-              purpose: "any"
-            },
-            {
-              src: "/icon-192-maskable.png",
-              sizes: "192x192",
-              type: "image/png",
-              purpose: "maskable"
-            },
-            {
-              src: "/icon-512-maskable.png",
-              sizes: "512x512",
-              type: "image/png",
-              purpose: "maskable"
+              type: "image/png"
             }
           ]
         }
